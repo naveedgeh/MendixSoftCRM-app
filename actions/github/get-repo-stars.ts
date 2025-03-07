7cf490d5a7c2d050682089fc1c17707c24b6d7a0
naveedgeh <mastermindgeh@gmail.com>
2025-03-07 15:14:48 +0500

    change all thing

--- actions/github/get-repo-stars.ts
+++ actions/github/get-repo-stars.ts
@@ -4,7 +4,7 @@ export default async function getGithubRepoStars(): Promise<number> {
   try {
     const response: AxiosResponse<any> = await axios.get(
       process.env.NEXT_PUBLIC_GITHUB_REPO_API ||
-        "https://api.github.com/repos/pdovhomilja/MendixSoftCRM-app",
+        "https://api.github.com/repos/naveedgeh/MendixSoftCRM-app",
       {
         headers: {
           Authorization: `token ${process.env.NEXT_PUBLIC_GITHUB_TOKEN}`,
